853de5f977a39be528737279323db446d47e6866
Drefetr <d@drefetr.net>
2017-11-21 14:22:45 +1300

    Add composer.json & angularify create template

--- www/app/create/create.controller.js
+++ www/app/create/create.controller.js
@@ -1,6 +1,32 @@
 angular
 .module('create')
 .controller('CreateController',
-    function CreateController() {
+    function CreateController($scope, $http) {
+        $scope.Message = {
+            MID: "",
+            MIDLength: 16,
+            Key: "",
+            KeyLength: 16,
+            TTL: 1440
+        };
+
+        $scope.GenerateKey = function() {
+            $scope.Message.Key = "";
+
+            while($scope.Message.Key.length < $scope.Message.KeyLength){
+                var r = Math.random();
+
+                $scope.Message.Key
+                    += (r < 0.1 ? Math.floor(r * 100) : String.fromCharCode(Math.floor(r * 26) + (r > 0.5 ? 97 : 65)));
+            }
+        }
+
+        $scope.GenerateKey();
+
+        $scope.Submit = function() {
+            $http.get('api/create').then(function(response) {
+                alert(response);
+            });
+        }
     }
 );
--- www/img.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Send a generated image to the browser
-create_image();
-exit();
-
-
-?>
